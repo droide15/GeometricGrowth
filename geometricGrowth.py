a9e3fcba7bfb4ebd1ad048a742b871f949f7a086
jorge nunez <nuniez.jorge@gmail.com>
2019-05-04 09:45:14 -0700

    started testings

--- geometricGrowth.py
+++ geometricGrowth.py
@@ -1,6 +1,6 @@
-creating collage object
-rows = 4
-cols = 5
+#%%
+rows = 5
+cols = 6
 hSpan = 1
 vSpan = 1
 if rows%2 == 0:
@@ -16,20 +16,72 @@ up = vMid
 right = hMid + hSpan - 1
 down = vMid + vSpan - 1
 growArea = None
-#############################
-hSpan
-vSpan
-left
-up
-right
-down
-#############################
+#%%
+print('hSpan: ', hSpan)
+print('vSpan: ', vSpan)
+print('left: ', left)
+print('up: ', up)
+print('right: ', right)
+print('down: ', down)
+print('growArea: ' , growArea)
+#%%
+hStart = hMid
+hEnd = hMid
+vStart = vMid
+vEnd = vMid
+if cols%2 == 0:
+    hEnd += 1
 
-stitch next images
-calculate growth direction
-calculate growth area
+if rows%2 == 0:
+    vEnd += 1
 
------------------------------
+growDir = '*'
+if cols >= rows:
+    growDir = 'V'
+else:
+    growDir = 'H'
+#%%
+print('growDir: ', growDir)
+print('hStart: ', hStart)
+print('hEnd: ', hEnd)
+print('vStart: ', vStart)
+print('vEnd: ', vEnd)
+#%%
+if growDir == '*':
+    hStart -= 1
+    hEnd += 1
+    vStart -= 1
+    vEnd += 1
+
+if cols >= rows:
+    if growDir == 'V':
+        growDir = 'H'
+    elif growDir == 'H':
+        growDir = '*'
+        if hStart - 1 > left:
+            growDir = 'V'
+        
+        hStart -= 1
+        hEnd += 1
+        vStart -= 1
+        vEnd += 1
+    else:
+        growDir = 'V'
+else:
+    if growDir == 'V':
+        growDir = '*'
+        if vStart - 1 > up:
+            growDir = 'H'
+        
+        hStart -= 1
+        hEnd += 1
+        vStart -= 1
+        vEnd += 1
+    elif growDir == 'H':
+        growDir = 'V'
+    else:
+        growDir = 'H'
+#%%
 growDir = '*'
 if growArea is not None:
     if cols-hSpan > rows-vSpan:
@@ -53,4 +105,3 @@ if growArea is not None:
         down += 1
 
 growArea = hSpan*vSpan
------------------------------
